f75d06a11569449136f971839ac3ea4d6344419a
Kirk Benell <kirk.benell@sparkfun.com>
2025-09-17 11:35:39 -0600

    initial impl of the uart driver - esp32 only - compiles - no test

---
+++ src/sfTk/sfDevFPC2534UART.cpp
@@ -0,0 +1,75 @@
+/*
+ *---------------------------------------------------------------------------------
+ *
+ * Copyright (c) 2025, SparkFun Electronics Inc.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ *---------------------------------------------------------------------------------
+ */
+
+#include "sfDevFPC2534UART.h"
+
+sfDevFPC2534UART::sfDevFPC2534UART() : _theUART{nullptr}
+{
+}
+
+//--------------------------------------------------------------------------------------------
+bool sfDevFPC2534UART::initialize(HardwareSerial &theUART)
+{
+    _theUART = &theUART;
+
+    return true;
+}
+
+//--------------------------------------------------------------------------------------------
+
+bool sfDevFPC2534UART::dataAvailable()
+{
+    if (_theUART == nullptr)
+        return false; // UART bus not initialized
+
+    return _theUART->available() > 0;
+}
+
+//--------------------------------------------------------------------------------------------
+void sfDevFPC2534UART::clearData()
+{
+    if (_theUART == nullptr)
+        return; // UART bus not initialized
+                // clear buffer
+    while (_theUART->available() > 0)
+        _theUART->read();
+}
+
+//--------------------------------------------------------------------------------------------
+uint16_t sfDevFPC2534UART::write(const uint8_t *data, size_t len)
+{
+    if (_theUART == nullptr)
+        return FPC_RESULT_IO_RUNTIME_FAILURE; // I2C bus not initialized
+
+    size_t nWritten = _theUART->write(data, len);
+    return nWritten == len ? FPC_RESULT_OK : FPC_RESULT_FAILURE;
+}
+
+//--------------------------------------------------------------------------------------------
+uint16_t sfDevFPC2534UART::read(uint8_t *data, size_t len)
+{
+    if (_theUART == nullptr)
+        return FPC_RESULT_IO_RUNTIME_FAILURE; // I2C bus not initialized
+
+    size_t readBytes = _theUART->available();
+    // Serial.printf("uart available %d bytes, requested = %d\n\r", readBytes, size);
+
+    if (readBytes == 0)
+        return FPC_RESULT_IO_NO_DATA; // No data available
+
+    readBytes = _theUART->readBytes(data, len);
+
+    // Serial.printf("uart read %d bytes, requested = %d\n\r", readBytes, size);
+
+    if (readBytes == 0 && len > 0)
+        return FPC_RESULT_IO_NO_DATA;
+
+    return FPC_RESULT_OK;
+}
---
+++ src/sfTk/sfDevFPC2534UART.h
@@ -0,0 +1,37 @@
+/*
+ *---------------------------------------------------------------------------------
+ *
+ * Copyright (c) 2025, SparkFun Electronics Inc.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ *---------------------------------------------------------------------------------
+ */
+
+#pragma once
+
+// from the FPC SDK
+#include "fpc_api.h"
+
+#include <Arduino.h>
+
+#include "sfDevFPC2534IComm.h"
+
+// TODO: With the RP2350/RP2040 port, a "arduino::" namespace is needed for HardwareSerial I belieave.
+//       Note for future port
+
+// uart impl for the FPC2534 communication class
+
+class sfDevFPC2534UART : public sfDevFPC2534IComm
+{
+  public:
+    sfDevFPC2534UART();
+    bool initialize(HardwareSerial &theUART);
+    bool dataAvailable();
+    void clearData();
+    uint16_t write(const uint8_t *data, size_t len);
+    uint16_t read(uint8_t *data, size_t len);
+
+  private:
+    HardwareSerial *_theUART;
+};
