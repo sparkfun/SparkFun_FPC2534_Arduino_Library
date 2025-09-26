baea80ae0560fbc3576b07b43665a3716f5d9f2e
Kirk Benell <kirk.benell@sparkfun.com>
2025-09-26 12:34:33 -0600

    cleanup; comments; finalization for initial testing

--- examples/Example01_Navigation/Example01_Navigation.ino
+++ examples/Example01_Navigation/Example01_Navigation.ino
@@ -17,43 +17,89 @@
 #define IRQ_PIN 11
 #define RST_PIN 12
 
-#define UART_RX 32
-#define UART_TX 14
+// #define UART_RX 32
+// #define UART_TX 14
 
+// State flags to manage sensor startup/state
 bool startNavigation = true;
 
+// Used to track LED state
 bool ledState = false;
 
 // Declare our sensor object
 SfeFPC2534I2C mySensor;
 
-/* Command callbacks */
+//------------------------------------------------------------------------------------
+// Callback functions the library calls
+//------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// on_error()
+//
+// Call if the sensor library detects/encounters an error
+//
 static void on_error(uint16_t error)
 {
     // hal_set_led_status(HAL_LED_STATUS_ERROR);
     Serial.printf("[ERROR]\t%d.\n\r", error);
 }
 
-static void on_status(uint16_t event, uint16_t state)
+//----------------------------------------------------------------------------
+// on_is_ready_change()
+//
+// Call when the device ready state changes
+//
+static void on_is_ready_change(bool isReady)
 {
+    // On startup the device isn't immediately ready. A message is sent when it is.
+    // The Library will call this function when that happens
 
-    // do we need to start navigation mode?
-    if (mySensor.currentMode() != STATE_NAVIGATION && mySensor.appIsReady())
+    if (isReady)
     {
-        Serial.printf("[SET MODE]\tNAVIGATION\n\r");
-        startNavigation = false;
-        fpc_result_t rc = mySensor.startNavigationMode(0);
-        if (rc != FPC_RESULT_OK)
-            Serial.printf("Failed to start navigation mode - error: %d\n\r", rc);
+        Serial.println("[STARTUP]\tFPC2534 Device is ready");
+
+        // do we need to start navigation mode?
+        if (mySensor.currentMode() != STATE_NAVIGATION && startNavigation)
+        {
+            // Place the sensor in Navigation mode and print out a menue.
+            startNavigation = false;
+            fpc_result_t rc = mySensor.startNavigationMode(0);
+
+            // error?
+            if (rc != FPC_RESULT_OK)
+            {
+                Serial.printf("[ERROR]\tFailed to start navigation mode - error: %d\n\r", rc);
+                return;
+            }
+
+            Serial.printf("[SETUP]\tSensor In Navigation mode.\n\r");
+            Serial.println();
+            Serial.println("\t- Swipe Up, Down, Left, Right to see events.");
+            Serial.println("\t- Press to toggle LED on/off.");
+            Serial.println("\t- Long Press to get firmware version.");
+            Serial.println();
+        }
+        else
+            Serial.println("[STATUS] \tFPC2534 Device is NOT ready");
     }
-    // Serial.printf("[STATUS]\tEvent: %d, State: 0x%04X\n\r", event, state);
 }
 
+//----------------------------------------------------------------------------
+// on_version()
+//
+// Call when the sensor sends a version string
+//
 static void on_version(char *version)
 {
-    Serial.printf("[VERSION]\t%s\n\r", version);
+    // just print the version string
+    Serial.printf("\t\t%s\n\r", version);
 }
 
+//----------------------------------------------------------------------------
+// on_navigation()
+//
+// Call when the sensor sends a navigation event
+//
 static void on_navigation(int gesture)
 {
     Serial.printf("[NAVIGATION]\t");
@@ -74,14 +120,17 @@ static void on_navigation(int gesture)
     case CMD_NAV_EVENT_LEFT:
         Serial.printf("LEFT\n\r");
         break;
-    case CMD_NAV_EVENT_PRESS: {
-        Serial.printf("PRESS ->{LED %s}\n\r", ledState ? "OFF" : "ON");
+    case CMD_NAV_EVENT_PRESS:
+        // Toggle the on-board  LED
+        Serial.printf("PRESS -> {LED %s}\n\r", ledState ? "OFF" : "ON");
         ledState = !ledState;
         mySensor.setLED(ledState);
         break;
-    }
+
     case CMD_NAV_EVENT_LONG_PRESS:
-        Serial.printf("LONG PRESS ->{Get Version}\n\r");
+        // Request the firmware version from the sensor. The sensor will respond
+        // with a version event that will call our on_version() function above.
+        Serial.printf("LONG PRESS -> {Get Version}\n\r");
         mySensor.requestVersion();
         break;
     default:
@@ -89,16 +138,18 @@ static void on_navigation(int gesture)
         break;
     }
 }
-static void on_finger_change(bool present)
-{
-    // Serial.printf("[FINGER]\t%s\n\r", present ? "PRESENT" : "NOT PRESENT");
-}
+
+// Define our command callbacks the library will call on events from the sensor
 static const sfDevFPC2534Callbacks_t cmd_cb = {.on_error = on_error,
-                                               .on_status = on_status,
                                                .on_version = on_version,
                                                .on_navigation = on_navigation,
-                                               .on_finger_change = on_finger_change};
+                                               .on_is_ready_change = on_is_ready_change};
 
+//------------------------------------------------------------------------------------
+// reset_sensor()
+//
+// Simple function to toggle the reset pin of the sensor
+//
 void reset_sensor(void)
 {
     // Reset the sensor by toggling the reset pin
@@ -109,9 +160,11 @@ void reset_sensor(void)
     delay(250);                  // Wait for sensor to initialize
 }
 
+//------------------------------------------------------------------------------------
+// setup()
+//
 void setup()
 {
-
     delay(2000);
 
     // Set up serial communication for debugging
@@ -121,6 +174,10 @@ void setup()
         ; // Wait for serial port to connect. Needed for native USB port only
     }
     Serial.println();
+    Serial.println("----------------------------------------------------------------");
+    Serial.println(" SparkFun FPC2534 Navigation Example - I2C");
+    Serial.println("----------------------------------------------------------------");
+    Serial.println();
 
     // Initialize the I2C communication
     Wire.begin();
@@ -128,39 +185,45 @@ void setup()
     // Reset the sensor to ensure it's in a known state - by default this also triggers the
     // sensor to send a status message
     reset_sensor();
-    // delay(1000);
+
+    // Is the sensor there - on the I2C bus?
     Wire.beginTransmission(kFPC2534DefaultAddress);
     if (Wire.endTransmission() != 0)
     {
-        Serial.println("Touch Sensor not found on I2C bus. HALT");
+        Serial.println("[ERROR]\tTouch Sensor FPC2534 not found on I2C bus. HALT");
         while (1)
         {
             delay(1000); // Wait indefinitely if device is not found
         }
     }
     else
-        Serial.println("Touch Sensor found on I2C bus.");
+        Serial.println("[STARTUP]\tTouch Sensor FPC2534 found on I2C bus");
 
-    // Initialize the sensor
+    // Initialize the sensor library
     if (!mySensor.begin(kFPC2534DefaultAddress, Wire, 0, IRQ_PIN))
     {
-        Serial.println("FPC2534 not found. Check wiring. HALT.");
+        Serial.println("[ERROR]\tFPC2534 not found. Check wiring. HALT.");
         while (1)
             delay(1000);
     }
-    Serial.println("FPC2534 initialized.");
+    Serial.println("[STARTUP]\tFPC2534 initialized.");
 
     // set the callbacks for the sensor library to call
     mySensor.setCallbacks(cmd_cb);
+
+    // One last reset of the sensor = observation shows that this is needed after the above device ping...
     reset_sensor();
 
-    Serial.println("Fingerprint system initialized.");
+    // Ready to go!
+    Serial.println("[STARTUP]\tFingerprint system initialized.");
 }
 
+//------------------------------------------------------------------------------------
 void loop()
 {
 
-    // Call the library to process the next response from the sensor
+    // Call the library to process the next response from the sensor. The library will call our above
+    // callback functions as events occur.
     fpc_result_t rc = mySensor.processNextResponse();
     if (rc != FPC_RESULT_OK && rc != FPC_PENDING_OPERATION)
     {
