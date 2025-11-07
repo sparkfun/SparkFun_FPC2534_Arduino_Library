f029a060a618f1f27aca6687b96ebb0bebf9992c
Kirk Benell <kirk.benell@sparkfun.com>
2025-11-07 14:32:55 -0700

    moved the ISR for new data logic into the underlying interface comm object - in prep for enabling SPI - which needs the same logic as I2C; Also added logic for ESP32 and RP2* boards so the IRS callback takes a paramter -- which will enable the use of multiple devices - if needed

--- src/sfTk/sfDevFPC2534IComm.h
+++ src/sfTk/sfDevFPC2534IComm.h
@@ -18,8 +18,28 @@
 class sfDevFPC2534IComm
 {
   public:
+    sfDevFPC2534IComm() : _dataAvailable{false}, _usingISRParam{true} {};
     virtual bool dataAvailable(void) = 0;
     virtual void clearData(void) = 0;
     virtual uint16_t write(const uint8_t *data, size_t len) = 0;
     virtual uint16_t read(uint8_t *data, size_t len) = 0;
+    // On SPI writes, the CS line needs to remain low during writes (which have multiple blocks).
+    // So add a normally no-op beginWrite and endWrite methods that can be overridden by SPI comm classes.
+    virtual void beginWrite(void) {};
+    virtual void endWrite(void) {};
+    void setISRDataAvailable(void);
+
+  protected:
+    // All communication protocols/types supported by the sensor use an interrupt to signal data availability.
+    // This is required for i2c and SPI interfaces (UART is okay b/c of Arduino Serial buffer handling). So
+    // we consolidate the core interrupt handling in this case. If needed, comm type specializations can use this.
+    void initISRHandler(uint32_t interruptPin);
+    bool isISRDataAvailable(void);
+
+    void clearISRDataAvailable(void);
+
+  private:
+    volatile bool _dataAvailable;
+    bool _usingISRParam;
 };
+
